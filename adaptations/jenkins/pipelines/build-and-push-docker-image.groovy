d38a20a06d76b3bfe6539f08525942e4d95a2b38
bmastahac <bogdan.mastahac@gmail.com>
2022-02-10 11:45:57 +0200

    update pipeline to use docker

--- adaptations/jenkins/pipelines/build-and-push-docker-image.groovy
+++ adaptations/jenkins/pipelines/build-and-push-docker-image.groovy
@@ -9,15 +9,16 @@
 // DATA_IMAGE_NAME - string
 // DATA_IMAGE_VERSION - string
 
-def docker_image_source = "$POD_DOCKER_REPOSITORY"+':'+"$DOCKER_AWS_JENKINS_DOCKER_IMAGE"+'_'+"$DOCKER_AWS_JENKINS_DOCKER_IMAGE_VERSION"
+def docker_image_source = "$POD_DOCKER_REPOSITORY"+'/'+"$DOCKER_AWS_JENKINS_DOCKER_IMAGE"+':'+"$DOCKER_AWS_JENKINS_DOCKER_IMAGE_VERSION"
 
 podTemplate(
     containers: [
-        containerTemplate(name: 'docker', image: docker_image_source, alwaysPullImage:true , ttyEnabled: true, command: 'cat')
+        containerTemplate(name: 'docker', image: docker_image_source, alwaysPullImage:false , ttyEnabled: true, command: 'cat')
     ],
     volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-    envVars: [secretEnvVar(key: 'aws_key_id', secretName: 'aws-config', secretKey: 'aws_key_id'),
-              secretEnvVar(key: 'aws_access_key', secretName: 'aws-config', secretKey: 'aws_access_key')
+    envVars: [secretEnvVar(key: 'DOCKER_USERNAME', secretName: 'docker-config', secretKey: 'docker_username'),
+              secretEnvVar(key: 'DOCKER_PASSWORD', secretName: 'docker-config', secretKey: 'docker_password'),
+              secretEnvVar(key: 'GITHUB_REPO_TOKEN', secretName: 'docker-config', secretKey: 'GITHUB_REPO_TOKEN'),
              ]
   ){
             podTemplate(
@@ -29,16 +30,11 @@ podTemplate(
 
                   stage ('Build and publish docker Image'){
                             container ('docker'){
-                                sh 'aws --version'
-                                sh 'aws configure set aws_access_key_id "$aws_key_id"'
-                                sh 'aws configure set aws_secret_access_key "$aws_access_key"'
-                                sh 'aws configure set default.region "eu-east-1"'
-                                sh 'aws configure set default.output \'NONE\''
-                                sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $POD_DOCKER_REPOSITORY'
+                                sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} '
                                     withFileParameter('dockerfile'){
                                         sh 'cat $dockerfile > Dockerfile'
-                                        sh 'docker build --no-cache --network host -t ${POD_DOCKER_REPOSITORY}:${DATA_IMAGE_NAME}_${DATA_IMAGE_VERSION} .'
-                                        sh 'docker push ${POD_DOCKER_REPOSITORY}:${DATA_IMAGE_NAME}_${DATA_IMAGE_VERSION}'
+                                        sh 'docker build --no-cache --build-arg GHCODE=${GITHUB_REPO_TOKEN} --network host -t ${POD_DOCKER_REPOSITORY}/${DATA_IMAGE_NAME}:${DATA_IMAGE_VERSION} .'
+                                        sh 'docker push ${POD_DOCKER_REPOSITORY}/${DATA_IMAGE_NAME}:${DATA_IMAGE_VERSION}'
 
                                     }
 
